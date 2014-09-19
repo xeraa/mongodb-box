610928a6e938b13eb063914c2ba9dd840744ad09
Philipp Krenn <pk@xeraa.net>
2014-09-19 22:12:24 +0200

    ADDED a text blob for a more realistic (and bigger) document

--- advanced/testdata.js
+++ advanced/testdata.js
@@ -5,6 +5,15 @@
 // Change to get the intended number of documents
 var documents = 10000;
 
+// A rather long text simulating the blog post — the same for all posts
+var text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+
+Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer";
+
 for(i = 0; i < documents; i++) {
 
      // Set some author names
@@ -28,7 +37,7 @@ for(i = 0; i < documents; i++) {
      var posted = new Date((ISODate().getTime() - 1000 * 60 * 60 * (ISODate().getTime() % 300)));
 
      // Insert the data
-     db.blogposts.insert({ "number": i, "name": name, "url": url, "posted": posted });
+     db.blogposts.insert({ "number": i, "name": name, "url": url, "posted": posted, "text": text });
 }
 
 print("Documents inserted: " + db.blogposts.count());
